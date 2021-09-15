6b2a1c706fee557ec971626d632d8f669a051794
mahedee <mahedee.hasan@gmail.com>
2021-09-15 19:42:26 +0600

    ...

--- DapperDemo/DapperDemo/Customer.API/Controllers/WeatherForecastController.cs
+++ DapperDemo/DapperDemo/Customer.API/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Customer.API.Controllers
+namespace Ordering.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
---
+++ DapperDemo/DapperDemo/Customer.API/Repositories/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Repositories
+{
+    public class BaseRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        protected BaseRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
