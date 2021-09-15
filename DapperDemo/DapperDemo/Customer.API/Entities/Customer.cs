deae48eae750863941ad3dbea463c7f85f3ead68
mahedee <mahedee.hasan@gmail.com>
2021-09-15 17:23:25 +0600

    ...

---
+++ DapperDemo/DapperDemo/Customer.API/Entities/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.API.Entities
+{
+    public class Customer
+    {
+    }
+}
