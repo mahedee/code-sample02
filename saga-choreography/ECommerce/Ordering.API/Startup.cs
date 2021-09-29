0d3754d9f197ca59f63f8899b98bea42030c72eb
mahedee <mahedee.hasan@gmail.com>
2021-09-29 17:48:51 +0600

    ...

--- saga-choreography/ECommerce/Ordering.API/Startup.cs
+++ saga-choreography/ECommerce/Ordering.API/Startup.cs
@@ -1,22 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Ordering.API.Db;
 using Plain.RabbitMQ;
 using RabbitMQ.Client;
-using Shared;
-using Shared.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ordering.API
 {
@@ -39,22 +30,7 @@ namespace Ordering.API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
             });
 
-
-            //services.AddSingleton<IConnectionProvider>(new ConnectionProvider("amqp://guest:guest@localhost:5672"));
-
-            // Configure IPublisher
-            //services.AddSingleton<IPublisher>(x => new Publisher(x.GetService<IConnectionProvider>(),
-            //    "order-exchange",
-            //    ExchangeType.Topic));
-
-            //services.AddMassTransit(config => {
-            //    config.UsingRabbitMq((ctx, cfg) =>
-            //    {
-            //        cfg.Host("amqp://guest:guest@localhost:5672");
-            //    });
-            //});
-
-            // Configure Sqlite
+            // Configure database
             services.AddDbContext<OrderingContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
 
@@ -72,28 +48,6 @@ namespace Ordering.API
                 ExchangeType.Topic));
 
             services.AddHostedService<CatalogResponseListener>();
-
-            // Configure rabbitmq
-            //services.AddMassTransit(x =>
-            //{
-            //    x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(config =>
-            //    {
-            //        config.Host(new Uri(Settings.RabbitMqRootUri), h =>
-            //        {
-            //            h.Username(Settings.RabbitMqUserName);
-            //            h.Password(Settings.RabbitMqPassword);
-            //        });
-
-            //    }));
-            //});
-
-            //Bus.Factory.CreateUsingInMemory(cfg =>
-            //{
-            //    cfg.UseJsonSerializer();
-            //});
-
-
-            //services.AddMassTransitHostedService();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -116,22 +70,6 @@ namespace Ordering.API
             {
                 endpoints.MapControllers();
             });
-
-
-            //var bus = Bus.Factory.CreateUsingRabbitMq(config =>
-            //{
-            //    config.Host("amqp://guest:guest@localhost:5672");
-            //    config.ReceiveEndpoint("temp-queue", c =>
-            //    {
-            //        c.Handler<Order>(ctx =>
-            //        {
-            //            return Console.Out.WriteLineAsync(ctx.Message.Name);
-            //        });
-            //    });
-            //});
-
-            //bus.Start();
-            //bus.Publish(new Order { Name = "Test Name" });
         }
     }
 }
