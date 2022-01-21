ff1143974ff65fb404544aa3f1243cfdb352a3b9
mahedee <mahedee.hasan@gmail.com>
2022-01-21 21:08:05 +0600

    ...

---
+++ token-based-auth/ECommerce/Ordering.API/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands;
+using Ordering.Application.DTOs;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpPost]
+        [ProducesDefaultResponseType(typeof(AuthResponseDTO))]
+        public async Task<IActionResult> Signin([FromBody] AuthCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
---
+++ token-based-auth/ECommerce/Ordering.Application/Common/Security/TokenGenerator.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Ordering.Application.Common.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Ordering.Application.Common.Security
+{
+    public class TokenGenerator : ITokenGenerator
+    {
+        private readonly string _userName = "mahedee";
+        private readonly string _password = "mahedee123";
+        private readonly string key;
+
+        public TokenGenerator(string key)
+        {
+            this.key = key;
+        }
+
+        public string GenerateToken(string userName, string password)
+        {
+            if (!(_userName.Equals(userName) || _password.Equals(password)))
+            {
+                return null;
+            }
+
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userName),
+                new Claim(JwtRegisteredClaimNames.Jti, userName),
+                new Claim(ClaimTypes.Name,userName),
+                new Claim("UserId",userName)
+            };
+
+
+            //claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var token = new JwtSecurityToken(
+                issuer: "jwt",
+                audience: "jwt",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: signingCredentials
+            );
+
+            var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodedToken;
+        }
+    }
+}
