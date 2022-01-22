2845bec7d919332e4349e64e77d35096a1d4e20c
mahedee <mahedee.hasan@gmail.com>
2022-01-22 12:34:43 +0600

    ..

--- token-based-auth/ECommerce/Ordering.API/Program.cs
+++ token-based-auth/ECommerce/Ordering.API/Program.cs
@@ -12,6 +12,7 @@ using Ordering.Core.Repositories.Command;
 using Ordering.Core.Repositories.Command.Base;
 using Ordering.Core.Repositories.Query;
 using Ordering.Core.Repositories.Query.Base;
+using Ordering.Infrastructure;
 using Ordering.Infrastructure.Data;
 using Ordering.Infrastructure.Identity;
 using Ordering.Infrastructure.Repository.Command;
@@ -57,24 +58,27 @@ builder.Services.AddAuthentication(x =>
 // Dependency injection with key
 builder.Services.AddSingleton<ITokenGenerator>(new TokenGenerator(key));
 
+// Include Infrastructur Dependency
+builder.Services.AddInfrastructure(builder.Configuration);
+
 
 
 // Configuration for Sqlite
-builder.Services.AddDbContext<OrderingContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<OrderingContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Register dependencies
 builder.Services.AddMediatR(typeof(CreateCustomerHandler).GetTypeInfo().Assembly);
 builder.Services.AddMediatR(typeof(UserCreateCommandHandler).GetTypeInfo().Assembly);
 
-builder.Services.AddScoped(typeof(IQueryRepository<>), typeof(QueryRepository<>));
-builder.Services.AddTransient<ICustomerQueryRepository, CustomerQueryRepository>();
-builder.Services.AddScoped(typeof(ICommandRepository<>), typeof(CommandRepository<>));
-builder.Services.AddTransient<ICustomerCommandRepository, CustomerCommandRepository>();
-builder.Services.AddScoped<IIdentityService, IdentityService>();
+//builder.Services.AddScoped(typeof(IQueryRepository<>), typeof(QueryRepository<>));
+//builder.Services.AddTransient<ICustomerQueryRepository, CustomerQueryRepository>();
+//builder.Services.AddScoped(typeof(ICommandRepository<>), typeof(CommandRepository<>));
+//builder.Services.AddTransient<ICustomerCommandRepository, CustomerCommandRepository>();
+//builder.Services.AddScoped<IIdentityService, IdentityService>();
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<OrderingContext>()
-    .AddDefaultTokenProviders();
+//builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+//    .AddEntityFrameworkStores<OrderingContext>()
+//    .AddDefaultTokenProviders();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ token-based-auth/ECommerce/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Core.Repositories.Command;
+using Ordering.Core.Repositories.Command.Base;
+using Ordering.Core.Repositories.Query;
+using Ordering.Core.Repositories.Query.Base;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Identity;
+using Ordering.Infrastructure.Repository.Command;
+using Ordering.Infrastructure.Repository.Command.Base;
+using Ordering.Infrastructure.Repository.Query;
+using Ordering.Infrastructure.Repository.Query.Base;
+using Ordering.Infrastructure.Services;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<OrderingContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(OrderingContext).Assembly.FullName)
+                ));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<OrderingContext>()
+            .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Default Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+                // Default Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+                // Default SignIn settings.
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+                options.User.RequireUniqueEmail = true;
+            });
+
+
+            services.AddScoped<IIdentityService, IdentityService>();
+
+            services.AddScoped(typeof(IQueryRepository<>), typeof(QueryRepository<>));
+            services.AddTransient<ICustomerQueryRepository, CustomerQueryRepository>();
+            services.AddScoped(typeof(ICommandRepository<>), typeof(CommandRepository<>));
+            services.AddTransient<ICustomerCommandRepository, CustomerCommandRepository>();
+
+
+            return services;
+        }
+    }
+}
