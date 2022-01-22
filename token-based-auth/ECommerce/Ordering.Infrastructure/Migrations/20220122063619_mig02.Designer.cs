938536ae1db4daea833c7799d381b706fd09f204
mahedee <mahedee.hasan@gmail.com>
2022-01-22 12:39:14 +0600

    ...

--- token-based-auth/ECommerce/Ordering.Infrastructure/Migrations/20220122063619_mig02.Designer.cs
+++ token-based-auth/ECommerce/Ordering.Infrastructure/Migrations/20220122063619_mig02.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ordering.Infrastructure.Data;
 
@@ -10,9 +11,10 @@ using Ordering.Infrastructure.Data;
 namespace Ordering.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderingContext))]
-    partial class OrderingContextModelSnapshot : ModelSnapshot
+    [Migration("20220122063619_mig02")]
+    partial class mig02
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
