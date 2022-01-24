4232cabb8fd3955131398426e685c6058e8b580f
mahedee <mahedee.hasan@gmail.com>
2022-01-24 17:44:53 +0600

    ....

---
+++ token-based-auth/ECommerce/Ordering.Application/Queries/User/GetAllUsersDetailsQuery.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Ordering.Application.Common.Interfaces;
+using Ordering.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Queries.User
+{
+    public class GetAllUsersDetailsQuery : IRequest<List<UserDetailsResponseDTO>>
+    {
+        //public string UserId { get; set; }
+    }
+
+    public class GetAllUsersDetailsQueryHandler : IRequestHandler<GetAllUsersDetailsQuery, List<UserDetailsResponseDTO>>
+    {
+        private readonly IIdentityService _identityService;
+
+        public GetAllUsersDetailsQueryHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<List<UserDetailsResponseDTO>> Handle(GetAllUsersDetailsQuery request, CancellationToken cancellationToken)
+        {
+            
+
+            var users = await _identityService.GetAllUsersAsync();
+            var userDetails = users.Select(x => new UserDetailsResponseDTO()
+            {
+                Id = x.id,
+                Email = x.email,
+                UserName = x.userName,
+                Roles = (IList<string>)_identityService.GetUserRolesAsync(x.id)
+            }).ToList();
+
+
+            return userDetails;
+            //throw new NotImplementedException();
+        }
+
+        //public async Task<UserDetailsResponseDTO> Handle(GetUserDetailsQuery request, CancellationToken cancellationToken)
+        //{
+        //    //var (userId, userName, email, roles ) = await _identityService.GetUserDetailsAsync(request.UserId);
+        //    //return new UserDetailsResponseDTO() { Id = userId, UserName = userName, Email = email, Roles = roles };
+        //}
+    }
+}
