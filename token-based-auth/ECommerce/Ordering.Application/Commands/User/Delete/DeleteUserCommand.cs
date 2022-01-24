8b609240e1e8368b2e541b325ef59ef9844e4a23
mahedee <mahedee.hasan@gmail.com>
2022-01-24 09:53:10 +0600

    ...

---
+++ token-based-auth/ECommerce/Ordering.Application/Commands/User/Delete/DeleteUserCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Ordering.Application.Common.Interfaces;
+
+namespace Ordering.Application.Commands.User.Delete
+{
+    public class DeleteUserCommand : IRequest<int>
+    {
+        public string Id { get; set; }
+    }
+
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, int>
+    {
+        private readonly IIdentityService _identityService;
+
+        public DeleteUserCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+        public async Task<int> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _identityService.DeleteUserAsync(request.Id);
+
+            return result ? 1 : 0;
+        }
+    }
+}
