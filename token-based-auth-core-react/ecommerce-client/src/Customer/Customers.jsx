ea1f679efd7410ff3ddcfd5c2912318d057250a0
mahedee <mahedee.hasan@gmail.com>
2022-01-26 15:55:26 +0600

    ...

---
+++ token-based-auth-core-react/ecommerce-client/src/Customer/Customers.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+export class Customers extends Component
+{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            customers: [],
+            loading: true,
+            failder: false,
+            error: ''
+        }
+    }
+
+    populateCustomersData(){
+        axios.get("api/Employees/GetEmployees").then(result => {
+            const response = result.data;
+            this.setState({customers: response, loading: false, error: ""});
+        }).catch(error => {
+            this.setState({customers: [], loading: false, failed: true, error: "Customers could not be loaded!"});
+        });
+    }
+
+
+}
