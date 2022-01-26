83063f45dd2b860fa1dc3e79559795594b156369
mahedee <mahedee.hasan@gmail.com>
2022-01-26 17:32:19 +0600

    ....

--- token-based-auth-core-react/ecommerce-client/src/Customer/Customers.jsx
+++ token-based-auth-core-react/ecommerce-client/src/Customer/Customers.jsx
@@ -5,6 +5,7 @@ export class Customers extends Component
 {
     constructor(props){
         super(props);
+        this.onCustomerCreate = this.onCustomerCreate.bind(this);
 
         this.state = {
             customers: [],
@@ -12,6 +13,13 @@ export class Customers extends Component
             failder: false,
             error: ''
         }
+    }
+
+    // Event handler for create button
+    onCustomerCreate()
+    {
+        const {history} = this.props;
+        history.push('/create');
     }
 
     populateCustomersData(){
@@ -24,4 +32,61 @@ export class Customers extends Component
     }
 
 
+    renderAllCustomerTable(employees){
+        return(
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        {/* <th>Mother's Name</th>
+                        <th>Date of Birth</th>
+                        <th>Actions</th> */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        employees.map(employee => (
+                            <tr key={employee.id}>
+                                <td>{employee.firstname}</td>
+                                <td>{employee.lastname}</td>
+                                <td>{employee.email}</td>
+                                {/* <td>{employee.fathersName}</td>
+                                <td>{employee.mothersName}</td>
+                                <td>{ new Date(employee.dateOfBirth).toISOString().slice(0,10)}</td>
+                                <td><button onClick={()=> this.OnEmployeeEdit(employee.id)}  className= "btn btn-success">Edit</button> || 
+                                <button onClick={()=> this.OnEmployeeDelete(employee.id)} className= "btn btn-danger">Delete</button></td> */}
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        );
+    }
+
+
+    render(){
+
+        let content = this.state.loading ? (
+            <p>
+                <em>Loading...</em>
+            </p>
+        ):(
+            this.renderAllCustomerTable(this.state.customers)
+        )
+
+        return(
+            <div>
+                <h2>Customers</h2>
+                <button onClick={()=> this.onCustomerCreate()} className="btn btn-primary">Create</button>
+                {content}
+            </div>
+        );
+    }
+
+
 }
+
+
+// https://blog.telexarsoftware.com/integrating-a-bootstrap-template-to-a-reactjs-application/
