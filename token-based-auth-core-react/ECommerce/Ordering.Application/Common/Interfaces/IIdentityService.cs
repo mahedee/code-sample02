35a7a3dff11ed16d1012ccb90b2fa6c62e1115e3
mahedee <mahedee.hasan@gmail.com>
2022-02-04 12:56:08 +0600

    ....

--- token-based-auth-core-react/ECommerce/Ordering.Application/Common/Interfaces/IIdentityService.cs
+++ token-based-auth-core-react/ECommerce/Ordering.Application/Common/Interfaces/IIdentityService.cs
@@ -32,6 +32,7 @@ namespace Ordering.Application.Common.Interfaces
         Task<bool> IsInRoleAsync(string userId, string role);
         Task<List<string>> GetUserRolesAsync(string userId);
         Task<bool> AssignUserToRole(string userName, IList<string> roles);
+        Task<bool> UpdateUsersRole(string userName, IList<string> usersRole);
 
 
     }
