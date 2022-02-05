540cc167c98bb152cc0b647eb6daed36ece634de
mahedee <mahedee.hasan@gmail.com>
2022-02-05 14:16:48 +0600

    ....

--- token-based-auth-core-react/ecommerce.client/src/components/Auth/Login.js
+++ token-based-auth-core-react/ecommerce.client/src/components/Auth/Login.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { Container } from "reactstrap";
 import LoginMenu from "../LoginMenu";
 import { postDataForLogin } from "../services/AccessAPI";
 import SessionManager from "./SessionManager";
