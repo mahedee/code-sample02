75e6d534cafdba10c34fcb6c4b1ef8101c5a753d
mahedee <mahedee.hasan@gmail.com>
2022-02-10 17:43:56 +0600

    .....

---
+++ health-check/HealthCheck/WebStatus/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure service for healthcheck
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Cofigure methods for healthcheck
+app.MapHealthChecksUI();
+
+app.MapControllers();
+
+app.Run();
