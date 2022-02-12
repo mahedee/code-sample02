58c5b4d81dffafc9a9286d0a40bfeb6b8c09ddd9
mahedee <mahedee.hasan@gmail.com>
2022-02-12 21:13:12 +0600

    ....

---
+++ health-check/HealthCheck/Customer/Utility/DatabaseHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Data.SqlClient;
+
+namespace Customer.API.Utility
+{
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        private IConfiguration _configuration;
+        public DatabaseHealthCheck(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            string _connectionString = _configuration.GetConnectionString("CustomerDBConnection");
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (SqlException)
+                {
+                    return HealthCheckResult.Healthy();
+                }
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
