07dbf9f3b16b46cfc66ed13f5294e79b534b1175
mahedee <mahedee.hasan@gmail.com>
2022-02-12 17:06:30 +0600

    ...

--- health-check/HealthCheck/Customer/Program.cs
+++ health-check/HealthCheck/Customer/Program.cs
@@ -1,11 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+/*
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{environment}.json", optional: true)
+    .AddEnvironmentVariables();
+*/
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string _connectionString = builder.Configuration.GetConnectionString("CustomerDBConnection");
 
 // Register required services for health checks
 builder.Services.AddHealthChecks();
