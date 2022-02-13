3b0530df87e0763d1bb137497d3e0d4d570d1b26
mahedee <mahedee.hasan@gmail.com>
2022-02-13 17:20:45 +0600

    ....

---
+++ api-gateway-ocelot/APIGateway/Catalog.API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "Soap", "Light", "Powder" };
+        }
+    }
+}
