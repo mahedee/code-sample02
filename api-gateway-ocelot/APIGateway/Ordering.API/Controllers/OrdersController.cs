9c09ec4458e9383db99eec87405fa4dc9dfa0c32
mahedee <mahedee.hasan@gmail.com>
2022-02-13 17:49:46 +0600

    ....

---
+++ api-gateway-ocelot/APIGateway/Ordering.API/Controllers/OrdersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "Order 01", "Order 02", "Order 03" };
+        }
+    }
+}
