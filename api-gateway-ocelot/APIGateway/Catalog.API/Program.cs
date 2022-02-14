29402fea9cde87ad62e9eeb9e70f6b511e8c9262
mahedee <mahedee.hasan@gmail.com>
2022-02-14 17:44:26 +0600

    ....

--- api-gateway-ocelot/APIGateway/Catalog.API/Program.cs
+++ api-gateway-ocelot/APIGateway/Catalog.API/Program.cs
@@ -1,8 +1,14 @@
+using Catalog.API.Db;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<CatalogContext>(opt => opt.UseInMemoryDatabase("CatalogDB"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
