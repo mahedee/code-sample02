bf3a9a0bbb740affb14d0117a1b8b7046b5497ad
mahedee <mahedee.hasan@gmail.com>
2022-02-28 14:29:19 +0600

    ....

---
+++ kubernetes-demo/KubernetesDemo/Catalog.API/Db/CatalogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Catalog.API.Models;
+
+namespace Catalog.API.Db
+{
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Catalog.API.Models.Product> Product { get; set; }
+    }
+}
---
+++ kubernetes-demo/KubernetesDemo/Catalog.API/Migrations/20220227063852_mig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.API.Migrations
+{
+    public partial class mig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AvailableStock = table.Column<int>(type: "int", nullable: false),
+                    RestockThreshold = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
