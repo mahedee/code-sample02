bfc17cad38e92eafe57af95fe79ea8fa6e3869f5
mahedee <mahedee.hasan@gmail.com>
2022-03-02 09:05:02 +0600

    ...

--- kubernetes-demo/KubernetesDemo/Catalog.API/Controllers/ProductsController.cs
+++ kubernetes-demo/KubernetesDemo/Catalog.API/Controllers/ProductsController.cs
@@ -1,13 +1,8 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Catalog.API.Db;
 using Catalog.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.API.Controllers
 {
