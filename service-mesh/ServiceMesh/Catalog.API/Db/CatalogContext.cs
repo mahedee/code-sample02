10ab3008445bd9205dd191940c074cce2ba0f4c2
mahedee <mahedee.hasan@gmail.com>
2022-03-02 15:59:42 +0600

    ...

---
+++ service-mesh/ServiceMesh/Catalog.API/Db/CatalogContext.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Db
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Product>().HasData(
+            //    new Product {Id = 1, Name = "Econo Pen", Description = "Econo", Price = 10, AvailableStock = 100, RestockThreshold = 20 },
+            //    new Product {Id = 2, Name = "Casio Calculator", Description = "Casio Calculator", Price = 500, AvailableStock = 100, RestockThreshold = 10 });
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ service-mesh/ServiceMesh/Location.API/Controllers/CountriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Location.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+      [HttpGet("GetAll")]
+      public IEnumerable<string> Get()
+        {
+            return new string[] {"America","Bangladesh", "Canada" };
+        }
+    }
+}
