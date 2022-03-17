fa2dde36af2030c6666f15f7902d57386f764211
mahedee <mahedee.hasan@gmail.com>
2022-03-17 18:46:00 +0600

    ...

--- oeclot-swagger/APIGateway/BFF.Web/Program.cs
+++ oeclot-swagger/APIGateway/BFF.Web/Program.cs
@@ -1,14 +1,25 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Cache.CacheManager;
-
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using BFF.Web.Config;
+using Ocelot.Provider.Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var routes = "Routes";
+
+builder.Configuration.AddOcelotWithSwaggerSupport(options => {
+    options.Folder = routes;
+});
+
+builder.Services.AddOcelot(builder.Configuration).AddPolly();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    //.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddOcelot(routes, builder.Environment)
     .AddEnvironmentVariables();
 
 
@@ -24,12 +35,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //For ocelot
-builder.Services.AddOcelot()
+//builder.Services.AddOcelot()
     
     // Added for caching
-    .AddCacheManager(x => {
-        x.WithDictionaryHandle();
-    });
+    //.AddCacheManager(x => {
+    //    x.WithDictionaryHandle();
+    //});
 
 var app = builder.Build();
 
@@ -37,15 +48,27 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwaggerUI();
     //app.UseSwaggerForOcelotUI();
 }
 
-app.UseOcelot();
+//app.UseOcelot();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    //string swaggerJsonBasePath = string.IsNullOrWhiteSpace(options.RoutePrefix) ? "." : "..";
+    //options.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "My API");
+    //options.SwaggerEndpoint($"{swaggerJsonBasePath}/customer/swagger/v1/swagger.json", "OrderProcessing.Customer");
+    //options.SwaggerEndpoint($"{swaggerJsonBasePath}/product/swagger/v1/swagger.json", "OrderProcessing.Product");
+    //options.DownstreamSwaggerEndPointBasePath = "/swagger/docs";
+    options.PathToSwaggerGenerator = "/swagger/docs";
+    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+
+}).UseOcelot().Wait();
 
 app.MapControllers();
 
