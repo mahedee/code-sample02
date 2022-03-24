11f7744cb3865f46ed665ceab160df21ddc39349
mahedee <mahedee.hasan@gmail.com>
2022-03-24 17:06:06 +0600

    ....

---
+++ hrm/HRM/HRM.API/Repository/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using HRM.API.Models;
+
+namespace HRM.API.Repository
+{
+    public interface IEmployeeRepository
+    {
+        public Task<IEnumerable<Employee>> SelectAllEmployees();
+        public Task<Employee> SelectEmployee(int id);
+        public Task<string> UpdateEmployee(int id, Employee employee);
+        public Task<string> SaveEmployee(Employee employee);
+        public Task<string> DeleteEmployee(int id);
+    }
+}
---
+++ hrm/HRM/HRM.API/Services/EmployeeService.cs
@@ -0,0 +1,75 @@
+﻿using HRM.API.Models;
+using HRM.API.Repository;
+
+namespace HRM.API.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        IEmployeeRepository _employeeRepository;
+        public EmployeeService(IEmployeeRepository repository)
+        {
+            _employeeRepository = repository;
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            try
+            {
+                return await _employeeRepository.SelectAllEmployees();
+            }
+            catch (Exception exp)
+            {
+                throw (exp);
+            }
+        }
+
+        public async Task<Employee> GetEmployee(int id)
+        {
+            try
+            {
+                return await _employeeRepository.SelectEmployee(id);
+            }
+            catch (Exception exp)
+            {
+                throw (exp);
+            }
+        }
+
+        public async Task<string> EditEmployee(int id, Employee employee)
+        {
+            try
+            {
+                return await _employeeRepository.UpdateEmployee(id, employee);
+            }
+            catch (Exception exp)
+            {
+                throw (exp);
+            }
+        }
+
+        public async Task<string> AddEmployee(Employee employee)
+        {
+            try
+            {
+                return await _employeeRepository.SaveEmployee(employee);
+            }
+            catch (Exception exp)
+            {
+                throw (exp);
+            }
+        }
+
+        public async Task<string> RemoveEmployee(int id)
+        {
+            try
+            {
+                return await _employeeRepository.DeleteEmployee(id);
+            }
+            catch (Exception exp)
+            {
+                throw (exp);
+            }
+        }
+
+    }
+}
