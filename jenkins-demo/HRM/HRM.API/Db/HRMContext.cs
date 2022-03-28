beff51fb54916b6b3c9cab15740629571c44b35b
mahedee <mahedee.hasan@gmail.com>
2022-03-28 17:20:18 +0600

    ....

---
+++ jenkins-demo/HRM/HRM.API/Db/HRMContext.cs
@@ -0,0 +1,14 @@
+﻿using HRM.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRM.API.Db
+{
+    public class HRMContext : DbContext
+    {
+        public HRMContext(DbContextOptions<HRMContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
