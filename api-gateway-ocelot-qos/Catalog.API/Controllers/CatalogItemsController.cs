24665c6c3f6cefccb37bc6c01656e5bfda46cada
mahedee <mahedee.hasan@gmail.com>
2022-12-15 15:32:14 +0600

    added qos app

---
+++ api-gateway-ocelot-qos/Catalog.API/Controllers/CatalogItemsController.cs
@@ -0,0 +1,127 @@
+﻿using Catalog.API.Db;
+using Catalog.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogItemsController : ControllerBase
+    {
+        private readonly CatalogContext _context;
+        private static int _count = 0;
+
+        public CatalogItemsController(CatalogContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CatalogItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CatalogItem>>> GetCatalogItems()
+        {
+            _count++;
+            if(_count <= 3)
+            {
+                // Sleep for 4 seconds
+                Thread.Sleep(4000);
+            }
+
+            if (_context.CatalogItems == null)
+            {
+                return NotFound();
+            }
+            return await _context.CatalogItems.ToListAsync();
+        }
+
+        // GET: api/CatalogItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CatalogItem>> GetCatalogItem(int id)
+        {
+            if (_context.CatalogItems == null)
+            {
+                return NotFound();
+            }
+            var catalogItem = await _context.CatalogItems.FindAsync(id);
+
+            if (catalogItem == null)
+            {
+                return NotFound();
+            }
+
+            return catalogItem;
+        }
+
+        // PUT: api/CatalogItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCatalogItem(int id, CatalogItem catalogItem)
+        {
+            if (id != catalogItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(catalogItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CatalogItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CatalogItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CatalogItem>> PostCatalogItem(CatalogItem catalogItem)
+        {
+            if (_context.CatalogItems == null)
+            {
+                return Problem("Entity set 'CatalogContext.CatalogItems'  is null.");
+            }
+            _context.CatalogItems.Add(catalogItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCatalogItem", new { id = catalogItem.Id }, catalogItem);
+        }
+
+        // DELETE: api/CatalogItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCatalogItem(int id)
+        {
+            if (_context.CatalogItems == null)
+            {
+                return NotFound();
+            }
+            var catalogItem = await _context.CatalogItems.FindAsync(id);
+            if (catalogItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.CatalogItems.Remove(catalogItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CatalogItemExists(int id)
+        {
+            return (_context.CatalogItems?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
