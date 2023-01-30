f92d8a6164f9a21506ceb30d75dbb287cf820dfe
mahedee <mahedee.hasan@gmail.com>
2023-01-30 18:19:07 +0600

    ...

---
+++ sd-docker-demo/BFF.Web/Config/AlterUpstream.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BFF.Web.Config
+{
+    public class AlterUpstream
+    {
+        public static string AlterUpstreamSwaggerJson(HttpContext context, string swaggerJson)
+        {
+            var swagger = JObject.Parse(swaggerJson);
+            // ... alter upstream json
+            return swagger.ToString(Formatting.Indented);
+        }
+    }
+}
