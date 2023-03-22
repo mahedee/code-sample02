804783426f306587f44ffda21b77c76b96688698
mahedee <mahedee.hasan@gmail.com>
2023-03-22 18:54:06 -0300

    ....

---
+++ IValidatebleObjectSln/IValidatebleObjectDemo/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IValidatebleObjectDemo
+{
+    public class Employee : IValidatableObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public int DesignationId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Id <= 0)
+            {
+                yield return new ValidationResult("Employee Id is a required filed!", new[] {"Id"});
+            }
+
+            if (String.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Employee Name is a required field!", new[] { "Name" });
+            }
+        }
+    }
+}
