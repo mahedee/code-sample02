59f230843398b0d5a45279a35be2c83c7c4d90da
mahedee <mahedee.hasan@gmail.com>
2024-01-18 14:14:50 -0400

    ....

---
+++ ConcurrencyDBToken/ConcurrencyHandling.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ConcurrencyHandling.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConcurrencyHandling.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public virtual DbSet<Account> Accounts { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // IsConcurrencyToken() marks the property as a concurrency token.
+            // This is used by EF Core to implement optimistic concurrency.
+            // EF Core will use the value of the property in the WHERE clause of UPDATE and DELETE statements.
+
+            modelBuilder.Entity<Account>()
+                .Property(p => p.RowVersion).IsConcurrencyToken();
+        }
+
+    }
+}
---
+++ ConcurrencyDBToken/ConcurrencyHandling.API/Migrations/20240117173349_initial-mig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcurrencyHandling.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    AccountID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    RowVersion = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
