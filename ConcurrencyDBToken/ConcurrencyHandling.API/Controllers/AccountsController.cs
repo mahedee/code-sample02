231b13a1671035c8ea7754297984b3867fa61cac
mahedee <mahedee.hasan@gmail.com>
2024-01-18 14:14:50 -0400

    ....

---
+++ ConcurrencyDBToken/ConcurrencyHandling.API/Controllers/AccountsController.cs
@@ -0,0 +1,136 @@
+﻿using ConcurrencyHandling.API.Data;
+using ConcurrencyHandling.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConcurrencyHandling.API.Controllers
+{
+
+    // Concurrency check using database generated token
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Accounts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Account>>> GetAccounts()
+        {
+            if (_context.Accounts == null)
+            {
+                return NotFound();
+            }
+            return await _context.Accounts.ToListAsync();
+        }
+
+        // GET: api/Accounts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Account>> GetAccount(int id)
+        {
+            if (_context.Accounts == null)
+            {
+                return NotFound();
+            }
+            var account = await _context.Accounts.FindAsync(id);
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return account;
+        }
+
+        // PUT: api/Accounts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAccount(int id, Account account)
+        {
+            if (id != account.AccountID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(account).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Accounts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Account>> PostAccount(Account account)
+        {
+            if (_context.Accounts == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Accounts'  is null.");
+            }
+            _context.Accounts.Add(account);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAccount", new { id = account.AccountID }, account);
+        }
+
+        // DELETE: api/Accounts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccount(int id, Account account)
+        {
+            if (_context.Accounts == null)
+            {
+                return NotFound();
+            }
+            //var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            _context.Accounts.Remove(account);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool AccountExists(int id)
+        {
+            return (_context.Accounts?.Any(e => e.AccountID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ConcurrencyDBToken/ConcurrencyHandling.API/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConcurrencyHandling.API.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Balance { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; } = new byte[8];
+    }
+}
