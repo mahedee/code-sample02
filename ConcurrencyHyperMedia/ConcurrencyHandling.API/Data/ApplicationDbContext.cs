837b2ddca179df4e87c33dfd4e86092d1f30e1d9
mahedee <mahedee.hasan@gmail.com>
2024-01-18 22:51:10 -0400

    ....

---
+++ ConcurrencyHyperMedia/ConcurrencyHandling.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ConcurrencyHandling.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConcurrencyHandling.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee>? Employee { get; set; }
+    }
+}
