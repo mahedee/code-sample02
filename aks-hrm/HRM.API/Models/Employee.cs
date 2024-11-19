fe28f98a6c295ba1fe9063628790141d5be24ff2
mahedee <mahedee.hasan@gmail.com>
2024-11-19 14:34:13 -0500

    ....

---
+++ aks-hrm/HRM.API/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace HRM.API.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
