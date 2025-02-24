04c6f0f2d414e7fd1aec3f4106b0dd4c503ad68a
mahedee <mahedee.hasan@gmail.com>
2025-02-23 21:03:02 -0500

    ....

---
+++ CacheRefreshFunction/CacheRefreshAzureFunction/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+
+namespace CacheRefreshAzureFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionsDebugger.Enable();
+
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
