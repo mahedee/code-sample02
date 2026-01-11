13a0f89befa552e2a2d79870eec044ab7e4bc1ec
mahedee <mahedee.hasan@gmail.com>
2026-01-10 22:34:53 -0500

    Commit everything by Mahedee at 2026-01-10 22:34:52

---
+++ MiddlewareExample/Middleware/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace MiddlewareExample.Middleware
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestResponseLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            // Log Request details
+            Console.WriteLine($"Incoming Request: {context.Request.Method} {context.Request.Path}");
+
+            await _next(context);
+
+            stopwatch.Stop();
+
+            // Log Response details
+            Console.WriteLine($"Outgoing Response: {context.Response.StatusCode} | Time Taken: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
